a85e4c0bb799e5b0f47b5ac11ec51e5cc175c560
EricLoBin <ekilobin@gmail.com>
2020-04-20 14:10:37 -0300

    [UPDATE] Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
     return (
@@ -14,6 +15,18 @@ const Dashboard: React.FC = () => {
                 <input placeholder="Digite o nome do repositório"/>
                 <button type="submit">Pesquisar</button>
             </Form>
+
+            <Repositories>
+                <a href="test">
+                    <img src="https://avatars0.githubusercontent.com/u/54367134?s=460&v=4" alt="EricLoBin"/>
+                    <div>
+                        <strong>EricLoBin/GoStack-Nivel-3-ReactApp</strong>
+                        <p>No description, website, or topics provided.</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
         </>
     );
 };
