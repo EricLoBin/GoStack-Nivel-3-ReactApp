b34f5a46cf735c6ec6d78105c69bc3b14b7af92f
EricLoBin <ekilobin@gmail.com>
2020-04-21 18:28:58 -0300

    [ADD/UPDATE] Estilizando Detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface repositoryParams {
     repository: string;
@@ -8,7 +13,54 @@ interface repositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<repositoryParams>();
 
-    return <h1>Repository: {params.repository}</h1>
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer"/>
+                <Link to="/">
+                    <FiChevronsLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars0.githubusercontent.com/u/54367134?v=4" alt="Avater"/>
+                    <div>
+                        <strong>EricLoBin</strong>
+                        <p>descrição do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Ftars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertos</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+
+                    <Link to={`issue`}>
+                        <div>
+                            <strong>jrhbrhrh</strong>
+                            <p>ghyhgdyidydy</p>
+                        </div>
+
+                        <FiChevronRight size={20} />
+                    </Link>
+
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
